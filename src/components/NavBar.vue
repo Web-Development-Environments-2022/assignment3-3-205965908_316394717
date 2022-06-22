cf32fdcd56510bf5a609a8daf382a223c24f99cc
ofadlon <or5690@gmail.com>
2022-06-22 11:52:43 +0300

    working

---
+++ src/components/NavBar.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <router-link :to="{ name: 'main' }" class="navbar-brand">The recipes of Grandma and others</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link :to="{ name: 'main' }" class="nav-link">Home <span class="sr-only">(current)</span>
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+               aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit"> Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <div id="nav">
+      <router-link :to="{ name: 'main' }">Vue Recipes</router-link>
+      |
+      <router-link :to="{ name: 'search' }">Search</router-link>
+      |
+      {{ !$root.store.username }}
+      <span v-if="!$root.store.username">
+        Guest:
+        <router-link :to="{ name: 'register' }">Register</router-link>|
+        <router-link :to="{ name: 'login' }">Login</router-link>|
+      </span>
+      <span v-else>
+        {{ $root.store.username }}: <button @click="Logout">Logout</button>|
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  methods: {
+    Logout() {
+      this.$root.store.logout();
+      this.$root.toast("Logout", "User logged out successfully", "success");
+      this.$router.push("/").catch(() => {
+        this.$forceUpdate();
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/RecipePreviewImage.vue
@@ -0,0 +1,25 @@
+<template lang="">
+      <img v-if="image_load" :src="imgSrc" alt="Recipe Image" class="image" style="width:100%"  />
+</template>
+<script>
+export default {
+  name: "RecipePreviewImage",
+  props: {
+    imgSrc: { type: String, default: "TODO" },
+    recipeTitle: { type: String, default: "TODO" },
+  },
+  data() {
+    return {
+      image_load: false,
+    };
+  },
+  mounted() {
+    this.axios.get(this.imgSrc).then((i) => {
+      this.image_load = true;
+    });
+  },
+};
+</script>
+<style scoped>
+
+</style>
