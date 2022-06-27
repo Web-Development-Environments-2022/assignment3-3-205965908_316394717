066b48870f99107fdd95367c85d3b507f97afbc1
ofadlon <or5690@gmail.com>
2022-06-27 09:47:05 +0300

    working on specific recipe

--- src/pages/RecipeViewPage.vue
+++ src/pages/RecipeViewPage.vue
@@ -1,43 +1,54 @@
 <template>
   <div class="container">
     <div v-if="recipe">
-      <div class="recipe-header mt-3 mb-4">
-        <h1>{{ recipe.title }}</h1>
-        <img :src="recipe.image" class="center" />
-      </div>
+      <Header :title="recipe.title"></Header>
       <div class="recipe-body">
         <div class="wrapper">
           <div class="wrapped">
-            <div class="mb-3">
-              <div>Ready in {{ recipe.readyInMinutes }} minutes</div>
-              <div>Likes: {{ recipe.popularity }} likes</div>
-            </div>
-            Ingredients:
-            <ul>
-              <li
-                v-for="(r, index) in recipe.extendedIngredients"
-                :key="index + '_' + r.id"
-              >
-                {{ r.original }}
-              </li>
-            </ul>
+            <recipe-preview-image :img-src="recipe.image"></recipe-preview-image>
           </div>
-          <div class="wrapped">
-            Instructions:
-            <ol>
-              <li v-for="s in recipe._instructions" :key="s.number">
-                {{ s.step }}
-              </li>
-            </ol>
+          <div class="wrapped ml-3">
+            <i class="fa-solid fa-bell-concierge fa-lg"></i><i> {{ recipe.servings }} servings</i><br>
+            <i class="fa fa-clock fa-lg"></i><i> {{ recipe.readyInMinutes }} minutes</i><br>
+            <i class="fa fa-thumbs-o-up fa-lg" aria-hidden="true"></i><i> {{ recipe.popularity }} likes</i><br>
+            <i class="fa-solid fa-seedling fa-lg"></i>
+            <i> - </i>
+            <i v-if="recipe.vegan === true" class="fa-solid fa-check fa-lg"></i>
+            <i v-else class="fa-solid fa-x fa-lg"></i>
+            <br>
+            <i class="fas fa-fish fa-lg"></i>
+            <i> - </i>
+            <i v-if="recipe.vegetarian === true" class="fa-solid fa-check fa-lg"></i>
+            <i v-else class="fa-solid fa-x fa-lg"></i>
+            <br>
+            <i class="fa-solid fa-wheat-awn-circle-exclamation fa-lg"></i>
+            <i> - </i>
+            <i v-if="recipe.glutenFree === true" class="fa-solid fa-check fa-lg"></i>
+            <i v-else class="fa-solid fa-x fa-lg"></i>
+            <br>
+            <i v-if="recipe.inventedBy !== ''"><strong>This recipe invented by: {{recipe.inventedBy}}</strong><br></i>
+            <i v-if="recipe.serveDay !== ''"><strong>The recipe serve day is: {{recipe.serveDay}}</strong><br></i>
+
+            <i v-if="recipe.hasViewed === true" class="fa-solid fa-eye fa-lg"></i>
+            <i v-else class="fa-solid fa-eye-slash fa-lg"></i>
+            <br>
+            <i v-if="recipe.isFavorite === true" class="fa-solid fa-star fa-lg"></i><br>
           </div>
         </div>
+        <br>
+        <h2>Instructions:</h2>
+
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
+import RecipePreviewImage from "@/components/RecipePreviewImage";
+
 export default {
+  components: { RecipePreviewImage, Header },
   data() {
     return {
       recipe: null
@@ -50,8 +61,9 @@ export default {
 
       try {
         response = await this.axios.get(`recipes/${this.$route.params.recipeId}`);
-
         if (response.status !== 200) this.$router.replace("/NotFound");
+        this.recipe = response.data;
+        console.log(this.recipe);
       } catch (error) {
         console.log("error.response.status", error.response.status);
         this.$router.replace("/NotFound");
