ae323084f69a8921526fddf114251ed6b9ae7bce
Or Fadlon <or5690@gmail.com>
2022-08-06 20:17:35 +0300

    fix amount issue

--- src/components/IngredientInputModal.vue
+++ src/components/IngredientInputModal.vue
@@ -13,10 +13,10 @@
       <div class="col">
         <div class="form-outline">
           <label class="form-label" for="form6Example1" style="margin-left: 0px;">
-            Amount: {{ $root.store.recipeInformation[dictKey].amount }}
+            Amount: {{ amount }}
           </label>
           <input type="range" min="0" max="1000" id="form6Example1" class="form-control"
-                 v-model="$root.store.recipeInformation[dictKey].amount">
+                 v-model="amount" @input="update">
         </div>
       </div>
       <div class="col">
@@ -44,8 +44,14 @@ export default {
   },
   data() {
     return {
-      dictKey: `Ingredient-${this.instructionId}-${this.ingredientCounter}`
+      dictKey: `Ingredient-${this.instructionId}-${this.ingredientCounter}`,
+      amount: 0
     };
+  },
+  methods: {
+    update(){
+      this.$root.store.recipeInformation[this.dictKey].amount = this.amount;
+    }
   }
 
 };
