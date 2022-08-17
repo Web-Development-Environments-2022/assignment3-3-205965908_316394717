27cfe40d0f44192a8e806fcc7b8e0ca4aca06a26
Elran Oren <elranor@post.bgu.ac.il>
2022-08-17 20:11:34 +0300

    add login input validations

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -5,18 +5,36 @@
       <b-form-group id="input-group-Username" label-cols-sm="3" label="Username:" label-for="Username">
         <b-form-input id="Username" v-model="$v.form.username.$model" type="text"
                       :state="validateState('username')"></b-form-input>
-        <b-form-invalid-feedback>Username is required</b-form-invalid-feedback>
+        <b-form-invalid-feedback v-if="!$v.form.username.required">
+          Username is required
+        </b-form-invalid-feedback>
+        <b-form-invalid-feedback v-else-if="!$v.form.username.length">
+          Username length should be between 3-8 characters long
+        </b-form-invalid-feedback>
+        <b-form-invalid-feedback v-if="!$v.form.username.alpha">
+          Username alpha
+        </b-form-invalid-feedback>
       </b-form-group>
 
       <b-form-group id="input-group-Password" label-cols-sm="3" label="Password:" label-for="Password">
         <b-form-input id="Password" type="password" v-model="$v.form.password.$model"
                       :state="validateState('password')"></b-form-input>
-        <b-form-invalid-feedback> Password is required</b-form-invalid-feedback>
+        <b-form-invalid-feedback v-if="!$v.form.password.required">
+          Password is required
+        </b-form-invalid-feedback>
+        <b-form-invalid-feedback
+          v-if="$v.form.password.required && !$v.form.password.length">
+          Have length between 5-10 characters long
+        </b-form-invalid-feedback>
+        <b-form-invalid-feedback
+          v-if="!$v.form.password.valid">
+          need at least one number and one special letter
+        </b-form-invalid-feedback>
       </b-form-group>
 
       <b-button type="submit" variant="primary" style="width:100px;display:block;" class="mx-auto w-100 mb-2">Login
       </b-button>
-      <b-button variant="danger"  class="mx-auto w-100" @click="$router.push('/register')">Register</b-button>
+      <b-button variant="danger" class="mx-auto w-100" @click="$router.push('/register')">Register</b-button>
       <div class="mt-2">
         Do not have an account yet?
         <router-link to="register"> Register in here</router-link>
@@ -29,7 +47,7 @@
 </template>
 
 <script>
-import { required } from "vuelidate/lib/validators";
+import { alpha, maxLength, minLength, required } from "vuelidate/lib/validators";
 import Header from "@/components/Header";
 
 export default {
@@ -47,10 +65,18 @@ export default {
   validations: {
     form: {
       username: {
-        required
+        required,
+        length: (u) => minLength(3)(u) && maxLength(8)(u),
+        alpha
       },
       password: {
-        required
+        required,
+        valid: function(value) {
+          const containsNumber = /[0-9]/.test(value);
+          const containsSpecial = /[#?!@$%^&*-]/.test(value);
+          return containsNumber && containsSpecial;
+        },
+        length: (p) => minLength(5)(p) && maxLength(10)(p)
       }
     }
   },
